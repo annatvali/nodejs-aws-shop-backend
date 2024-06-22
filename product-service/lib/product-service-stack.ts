2b640d228092d8d330b9af25e248efebf47d5658
annatvali <anntvaliashvili@gmail.com>
2024-06-23 00:58:06 +0400

    feat: add AWS CDK stack for product service with DynamoDB tables & lambda integration

--- product-service/lib/product-service-stack.ts
+++ product-service/lib/product-service-stack.ts
@@ -2,30 +2,58 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 
 export class ProductServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const productsTable = new dynamodb.Table(this, 'ProductsTable', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      tableName: 'products',
+    });
+
+    const stocksTable = new dynamodb.Table(this, 'StocksTable', {
+      partitionKey: {
+        name: 'product_id',
+        type: dynamodb.AttributeType.STRING,
+      },
+      tableName: 'stocks',
+    });
+
     const getProductsListFunction = new lambda.Function(
       this,
-      "GetProductsListHandler",
+      'GetProductsListHandler',
       {
         runtime: lambda.Runtime.NODEJS_20_X,
-        code: lambda.Code.fromAsset("lambdas"),
-        handler: "getProductsList.handler",
+        code: lambda.Code.fromAsset('lambdas'),
+        handler: 'getProductsList.handler',
+        environment: {
+          PRODUCTS_TABLE: productsTable.tableName,
+          STOCKS_TABLE: stocksTable.tableName,
+        },
       }
     );
 
     const getProductByIdFunction = new lambda.Function(
       this,
-      "GetProductByIdHandler",
+      'GetProductByIdHandler',
       {
         runtime: lambda.Runtime.NODEJS_20_X,
-        code: lambda.Code.fromAsset("lambdas"),
-        handler: "getProductById.handler",
+        code: lambda.Code.fromAsset('lambdas'),
+        handler: 'getProductById.handler',
+        environment: {
+          PRODUCTS_TABLE: productsTable.tableName,
+          STOCKS_TABLE: stocksTable.tableName,
+        },
       }
     );
+
+    productsTable.grantReadData(getProductsListFunction);
+    stocksTable.grantReadData(getProductsListFunction);
+
+    productsTable.grantReadData(getProductByIdFunction);
+    stocksTable.grantReadData(getProductByIdFunction);
 
     const api = new apigateway.RestApi(this, 'ProductsApi', {
       restApiName: 'Products Service',
