a4c382a55f7cdf257544a0595022172b79c7bdd9
annatvali <anntvaliashvili@gmail.com>
2024-07-11 03:03:52 +0400

    feat: update importFileParser lambda to send each CSV record into SQS

--- import-service/lambdas/importFileParser.ts
+++ import-service/lambdas/importFileParser.ts
@@ -1,25 +1,72 @@
 import { S3Event } from 'aws-lambda';
-import { S3 } from '@aws-sdk/client-s3';
-import { GetObjectCommand } from '@aws-sdk/client-s3';
+import { S3Client, GetObjectCommand, CopyObjectCommand, DeleteObjectCommand } from '@aws-sdk/client-s3';
+import { SQSClient, SendMessageCommand } from '@aws-sdk/client-sqs';
 import { Readable } from 'stream';
-import { text } from 'stream/consumers';
+import csvParser from 'csv-parser';
 
-const s3Client = new S3();
+type ProductInventory = {
+  title: string;
+  description: string;
+  price: number;
+  count: number;
+};
 
 export const handler = async (event: S3Event) => {
+  const s3Client = new S3Client({});
+  const sqsClient = new SQSClient({});
+
   for (const record of event.Records) {
     const bucket = record.s3.bucket.name;
     const key = decodeURIComponent(record.s3.object.key.replace(/\+/g, ' '));
 
     try {
-      const command = new GetObjectCommand({ Bucket: bucket, Key: key });
-      const { Body } = await s3Client.send(command);
+      const getCommand = new GetObjectCommand({ Bucket: bucket, Key: key });
+      const { Body } = await s3Client.send(getCommand);
 
-      if (Body) {
-        const readableBody = Body as Readable;
-        const content = await text(readableBody);
-        console.log(content);
-      }
+      if (!Body) throw new Error('Body is undefined');
+
+      const readableStream = Body as Readable;
+
+      await new Promise<void>((resolve, reject) => {
+        readableStream
+          .pipe(csvParser())
+          .on('data', async (data) => {
+            try {
+              const msgCommand = new SendMessageCommand({
+                QueueUrl: process.env.SQS_URL,
+                MessageBody: JSON.stringify(data),
+              });
+              await sqsClient.send(msgCommand);
+            } catch (error) {
+              console.error('Error sending message to SQS:', error);
+              reject(error);
+            }
+          })
+          .on('end', () => {
+            console.log('CSV processing completed.');
+            resolve();
+          })
+          .on('error', (error) => {
+            console.error('Error parsing CSV:', error);
+            reject(error);
+          });
+      });
+
+      const newKey = key.replace('uploaded', 'parsed');
+      const copyCommand = new CopyObjectCommand({
+        Bucket: bucket,
+        CopySource: `${bucket}/${key}`,
+        Key: newKey,
+      });
+      await s3Client.send(copyCommand);
+      console.log('File copied from uploaded to parsed:', newKey);
+
+      const deleteCommand = new DeleteObjectCommand({
+        Bucket: bucket,
+        Key: key,
+      });
+      await s3Client.send(deleteCommand);
+      console.log(`Object ${key} was successfully moved to parsed folder`);
     } catch (error) {
       console.error(
         `Error processing file ${key} from bucket ${bucket}:`,
