30089461f2eaeaecce641aca4988ca03fa044dde
annatvali <anntvaliashvili@gmail.com>
2024-07-18 03:01:01 +0400

    feat: add environment variables to Lambda function in CDK stack

--- authorization-service/lib/authorization-service-stack.ts
+++ authorization-service/lib/authorization-service-stack.ts
@@ -1,16 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 export class AuthorizationServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+      const USER_NAME = process.env.USER_NAME as string;
+      const PASSWORD = process.env.PASSWORD as string;
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AuthorizationServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const basicAuthorizer = new lambda.Function(this, 'BasicAuthorizer', {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: 'basicAuthorizer.handler',
+      code: lambda.Code.fromAsset('lambdas'),
+      environment: {
+        USER_NAME:  USER_NAME,
+        PASSWORD: PASSWORD,
+      },
+    });
   }
 }
